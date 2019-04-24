66be8a3d9c6eb456ea1aa687a857a4cd8b1c31df
dejwkubikson <44845403+dejwkubikson@users.noreply.github.com>
2019-04-24 12:09:29 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+Template.drag.helpers({
+});
+
+
+Template.drag.events({
+	
+});
+
+Template.drag.rendered = function() {
+	
+}
